87f2fad70b5b630aab16eb2977232a40a8c55ed5
Dimon <bolilui.dmytro@gmail.com>
2023-01-21 02:44:53 +0200

    Add handlebars patterns for counties markup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './css/styles.css';
-import { fetchCountries } from './JS/fetchCountries';
-// import countriesCardsTpl from './templates/countries-cards.hbs';
+import { fetchCountries } from './JS/components/fetchCountries';
+import countriesTemplate from './templates/countries-cards.hbs';
+import countryTemplate from './templates/country.hbs';
 
 var debounce = require('lodash.debounce');
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
@@ -8,7 +9,8 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const DEBOUNCE_DELAY = 300;
 
 const inputEl = document.querySelector('#search-box');
-const countriesEl = document.querySelector('.country-info');
+const countryListEl = document.querySelector('.country-list');
+const countryEl = document.querySelector('.country-info');
 
 inputEl.addEventListener(
   'input',
@@ -17,10 +19,18 @@ inputEl.addEventListener(
     let countryName = event.target.value.trim();
 
     // check if country name has characters
-    if (countryName.length === 0) return;
+    if (countryName.length === 0) {
+      countryEl.innerHTML = '';
+      countryListEl.innerHTML = '';
+      return;
+    }
 
     // call function get countries data
     fetchCountries(countryName).then(countries => {
+      // clear content
+      countryEl.innerHTML = '';
+      countryListEl.innerHTML = '';
+
       if (countries.length > 10) {
         Notify.info(
           'Too many matches found. Please enter a more specific name.'
@@ -30,36 +40,53 @@ inputEl.addEventListener(
       // * from 2 - 10
       if (countries.length <= 10 && countries.length >= 2) {
         // console.log(countries);
+        // console.log(createCountriesMarkup(countries));
+        // const countriesMarkup = createCountriesMarkup(countries);
+        // countriesEl.innerHTML = countriesMarkup;
 
-        console.log(createCountriesMarkup(countries));
-        const countriesMarkup = createCountriesMarkup(countries);
-        countriesEl.innerHTML = countriesMarkup;
+        countryEl.innerHTML = '';
+        const countriesMarkup = countries
+          .map(country => {
+            // console.log(country);
+            return countriesTemplate(country);
+          })
+          .join('');
+
+        // console.log(countriesMarkup);
+
+        countryListEl.innerHTML = countriesMarkup;
       }
 
-      // if (countries.length === 1) {
-      //   console.log(countries[0]);
+      if (countries.length === 1) {
+        console.log(countries[0]);
 
-      //   const country = countries[0];
-      // }
+        countryListEl.innerHTML = '';
+        const country = countries[0];
+        console.log(countryTemplate(country));
+        const countryMarkup = countryTemplate(country);
+
+        countryEl.innerHTML = countryMarkup;
+      }
     });
 
     //
   }, DEBOUNCE_DELAY)
 );
 
-function createCountriesMarkup(countries) {
-  return countries
-    .map(({ flags, name }) => {
-      return `
-            <li class='countries__item'>
-              <img
-                src='${flags.svg}'
-                alt='${name.official}'
-                class='countries__flag'
-              />
-              <p class='countries__name'>${name.official}</p>
-            </li>
-          `;
-    })
-    .join('');
-}
+// ! create markup for countries with function
+// function createCountriesMarkup(countries) {
+//   return countries
+//     .map(({ flags, name }) => {
+//       return `
+//             <li class='countries__item'>
+//               <img
+//                 src='${flags.svg}'
+//                 alt='${name.official}'
+//                 class='countries__flag'
+//               />
+//               <p class='countries__name'>${name.official}</p>
+//             </li>
+//           `;
+//     })
+//     .join('');
+// }
